c31d9c0d2433fe1d4284744ae45912158028befb
Niya Keranova <nkeranova@axway.com>
2018-06-29 16:29:12 +0300

    RDPP-4802: sample data changes of the productdb and some updates of the endpints

--- 01_demo_setup/project/mysql/productdb.sql
+++ 01_demo_setup/project/mysql/productdb.sql
@@ -1,5 +1,5 @@
 USE productdb;
-
+ 
 CREATE TABLE products(
     id INT NOT NULL AUTO_INCREMENT,
     sku nvarchar(255),
@@ -7,12 +7,12 @@ CREATE TABLE products(
     description nvarchar(255),
     PRIMARY KEY(id)
 );
-
+ 
 INSERT INTO products
-VALUES ('1', 'API Builder', 'product-service', 'This service is part of the API Builder demo services.');
-
+VALUES ('1', 'SKU1', 'Spicy World Peppercorn', 'If we had to select just one spice to flavor our food, pepper, the master spice ,would be a wise choice.');
+ 
 INSERT INTO products
-VALUES ('2', 'API Builder', 'review-service', 'This service is part of the API Builder demo services.');
-
+VALUES ('2', 'SKU2', 'VAHDAM', 'Green Tea Leaves. The Himalayan region is blessed with one-of-a-kind soil type and combined with its presence near the Tropic of Cancer and the climatic conditions are ideal for the cultivation of tea.');
+ 
 INSERT INTO products
-VALUES ('3', 'API Builder', 'aggregation-service', 'This service is part of the API Builder demo services.');
+VALUES ('3', 'SKU3', 'Rani Garam Masala', 'Traditionally from Northern Indian, garam masala is a staple spice in Indian cookery. Garam, when translated means “warm” best describes the properties of the blend. ');
