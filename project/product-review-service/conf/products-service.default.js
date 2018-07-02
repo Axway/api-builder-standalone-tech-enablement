41d0de7feb0c6caa32abd8469d612e5694a583f8
Plamen Todorov <ptodorov@axway.com>
2018-07-02 22:37:08 +0100

    consuming product service

---
+++ project/product-review-service/conf/products-service.default.js
@@ -0,0 +1,11 @@
+module.exports = {
+	"pluginConfig": {
+		"@axway/api-builder-plugin-fn-swagger": {
+			"products-service": {
+				"x-vendor-openapi-authtype": "basic",
+				"x-vendor-openapi-username": process.env.PRODUCT_APIKEY,
+				"x-vendor-openapi-password": process.env.PRODUCT_PASSWORD || ""
+			}
+		}
+	}
+};
