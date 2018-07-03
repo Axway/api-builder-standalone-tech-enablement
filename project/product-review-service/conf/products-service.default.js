31b02fa8d924cfad4f12ffc0ff7991061fc11cd4
Plamen Todorov <ptodorov@axway.com>
2018-07-03 10:44:58 +0100

    product-review service - clean up test and deps

--- project/product-review-service/conf/products-service.default.js
+++ project/product-review-service/conf/products-service.default.js
@@ -2,7 +2,9 @@ module.exports = {
 	"pluginConfig": {
 		"@axway/api-builder-plugin-fn-swagger": {
 			"products-service": {
-				"x-vendor-openapi-authtype": "basic",
+				"x-vendor-openapi-authtype": process.env.AUTHTYPE || "basic",
+				//"x-vendor-openapi-key": process.env.PRODUCT_APIKEY,
+				//"x-vendor-openapi-token": process.env.PRODUCT_TOKEN,
 				"x-vendor-openapi-username": process.env.PRODUCT_APIKEY,
 				"x-vendor-openapi-password": process.env.PRODUCT_PASSWORD || "",
 				'x-vendor-openapi-uri': {
--- project/product-review-service/conf/review-service.default.js
+++ project/product-review-service/conf/review-service.default.js
@@ -2,7 +2,9 @@ module.exports = {
 	"pluginConfig": {
 		"@axway/api-builder-plugin-fn-swagger": {
 			"review-service": {
-				"x-vendor-openapi-authtype": "basic",
+				"x-vendor-openapi-authtype": process.env.AUTHTYPE || "basic",
+				//"x-vendor-openapi-key": process.env.REVIEW_APIKEY,
+				//"x-vendor-openapi-token": process.env.REVIEW_TOKEN,
 				"x-vendor-openapi-username": process.env.REVIEW_APIKEY,
 				"x-vendor-openapi-password": process.env.REVIEW_PASSWORD || "",
 				'x-vendor-openapi-uri': {
