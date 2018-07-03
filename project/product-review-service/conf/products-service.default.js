31b679803ce14c87602070369f90d67747041c4a
Plamen Todorov <ptodorov@axway.com>
2018-07-03 06:57:49 +0100

    envvars

--- project/product-review-service/conf/products-service.default.js
+++ project/product-review-service/conf/products-service.default.js
@@ -4,7 +4,11 @@ module.exports = {
 			"products-service": {
 				"x-vendor-openapi-authtype": "basic",
 				"x-vendor-openapi-username": process.env.PRODUCT_APIKEY,
-				"x-vendor-openapi-password": process.env.PRODUCT_PASSWORD || ""
+				"x-vendor-openapi-password": process.env.PRODUCT_PASSWORD || "",
+				'x-vendor-openapi-uri': {
+					'host': process.env.PRODUCT_HOST,
+					'port': parseInt(process.env.PRODUCT_PORT)
+				}				
 			}
 		}
 	}
